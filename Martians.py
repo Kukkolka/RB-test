64e424a769306f893dc2f5b399f595caa87ec4fe
unknown <kukkolka@gmail.com>
2019-04-08 16:40:50 +0100

    directions setup

--- Martians.py
+++ Martians.py
@@ -5,13 +5,47 @@ Created on Mon Apr  8 15:16:58 2019
 @author: Ilya Petin
 """
 
-class Martians:
+class Directions:
+    def __init__(self, dirs):
+        self.directions = dirs
+        self.max = len(dirs)
+        
+    def __iter__(self):
+        self.n = 0
+        return self
+    
+    def __next__(self):
+        self.n += 1
+        if self.n >= self.max:
+            self.n = 0         
+        return self.pos()
+            
+    def __prev__(self):    
+        self.n -= 1
+        if self.n < 0: 
+            self.counter_clockwise()
+        return self.pos()
+    
+    def counter_clockwise(self):
+        self.n = self.max - 1
+        return self.n
+    
+    def clockwise(self):
+        self.n = 0 
+        return self.n
+    
+    def pos(self):
+        return self.directions[self.n]  
+        
+
+    
+class Martians:       
     
     def main(self):
         print('starting martians')
         
     def setup(self):
-        return 1 
+        self.directions = iter(Directions(["N", "S", "E", "W"]))
 
 if __name__ == '__main__':
     game = Martians()
--- TestMartians.py
+++ TestMartians.py
@@ -9,12 +9,28 @@ from Martians import Martians
 
 
 class TestMartianInit(unittest.TestCase):
-    martians = Martians()
+    game = Martians()
+    expected_pos = ["N", "S", "E", "W"]
     
-    def test_setup(self):        
-        self.assertEqual(self.martians.setup(),1)
+    def test_directions(self):
+        self.game.setup()
+        self.assertEqual(self.game.directions.pos(),self.expected_pos[0])
         
-    
+    def test_clockwise_rotation(self):
+        self.game.setup()
+        self.assertEqual(next(self.game.directions),self.expected_pos[1])
+        self.assertEqual(next(self.game.directions),self.expected_pos[2])
+        self.assertEqual(next(self.game.directions),self.expected_pos[3])
+        self.assertEqual(next(self.game.directions),self.expected_pos[0])
+        
+    def test_counter_clockwise_ratation(self):        
+        self.game.setup()
+        self.assertEqual(self.game.directions.__prev__(),self.expected_pos[3])
+        self.assertEqual(self.game.directions.__prev__(),self.expected_pos[2])
+        self.assertEqual(self.game.directions.__prev__(),self.expected_pos[1])
+        self.assertEqual(self.game.directions.__prev__(),self.expected_pos[0])      
+        self.assertEqual(self.game.directions.__prev__(),self.expected_pos[3])
+        
 class TestMartians:    
     def main(self):
        unittest.main()    
